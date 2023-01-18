edd6eb80b162bce79d1abd8def911fad2fc9c24d
fullstack900 <Salman.upwork@devbatch.com>
2023-01-18 16:24:54 +0500

    intializing project

--- roofbuilders-node/config.js
+++ roofbuilders-node/config.js
@@ -32,12 +32,4 @@ export default {
   mongoUrl,
   mongoDbName,
   fastify: {logger:true},
-  s3: {
-    url:process.env.S3_URL || 'https://myroofbuilder-dev.nyc3.digitaloceanspaces.com',
-    urlCdn:process.env.S3_URL_CDN ||'https://viceroy-staging.nyc3.cdn.digitaloceanspaces.com',
-    bucket:process.env.S3_BUCKET ||'myroofbuilder-dev',
-    region:process.env.S3_REGION ||'nyc3',
-    accessKeyId:process.env.S3_ACCESS_KEY_ID || '7TXWOTSAIHRXZBAAT5D3',
-    secretAccessKey:process.env.S3_SECRET_ACCESS_KEY || 'mnzCgVuHEX6aEROP6zYfDJTuYkfcn7k2+Jh1fQnnD7M'
-  }
 };
